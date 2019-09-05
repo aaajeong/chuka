37dab56e8f9d6a367f1285b2d3e36e4bda1f5925
Ahjeong Park <qkrdkwjd59@naver.com>
2019-09-05 19:58:31 +0900

    Show highlight video list

--- logo_detection/HighlightWindow.py
+++ logo_detection/HighlightWindow.py
@@ -5,104 +5,112 @@
 # Created by: PyQt5 UI code generator 5.12.2
 #
 # WARNING! All changes made in this file will be lost!
-import sys
-from PyQt5 import QtCore, QtGui, QtWidgets
-#from SelectWindow import Ui_SelectWindow
-#from Select import MyApp
 
 
-class Ui_HighlightWindow(object):
+from PyQt5.QtCore import Qt
+from PyQt5 import QtCore
+from PyQt5.QtGui import QIcon
+from PyQt5.QtWidgets import *
 
-    def openWindow(self):
-        self.window = QtWidgets.QMainWindow()
-        self.ui = MyApp()
-        self.ui.setupUi(self.window)
-        self.window.show()
 
-    def setupUi(self, HighlightWindow):
-        HighlightWindow.setObjectName("HighlightWindow")
-        HighlightWindow.resize(640, 480)
-        icon = QtGui.QIcon()
-        icon.addPixmap(QtGui.QPixmap("./images/soccer.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
-        HighlightWindow.setWindowIcon(icon)
-        HighlightWindow.setUnifiedTitleAndToolBarOnMac(False)
-        self.centralwidget = QtWidgets.QWidget(HighlightWindow)
-        self.centralwidget.setObjectName("centralwidget")
-        self.horizontalLayoutWidget = QtWidgets.QWidget(self.centralwidget)
-        self.horizontalLayoutWidget.setGeometry(QtCore.QRect(10, 10, 381, 81))
-        self.horizontalLayoutWidget.setObjectName("horizontalLayoutWidget")
-        self.horizontalLayout = QtWidgets.QHBoxLayout(self.horizontalLayoutWidget)
-        self.horizontalLayout.setContentsMargins(0, 0, 0, 0)
-        self.horizontalLayout.setObjectName("horizontalLayout")
-        self.label = QtWidgets.QLabel(self.horizontalLayoutWidget)
-        self.label.setObjectName("label")
-        self.horizontalLayout.addWidget(self.label)
-        self.horizontalLayoutWidget_2 = QtWidgets.QWidget(self.centralwidget)
-        self.horizontalLayoutWidget_2.setGeometry(QtCore.QRect(400, 10, 211, 81))
-        self.horizontalLayoutWidget_2.setObjectName("horizontalLayoutWidget_2")
-        self.horizontalLayout_2 = QtWidgets.QHBoxLayout(self.horizontalLayoutWidget_2)
-        self.horizontalLayout_2.setContentsMargins(0, 0, 0, 0)
-        self.horizontalLayout_2.setObjectName("horizontalLayout_2")
-        self.pushButton = QtWidgets.QPushButton(self.horizontalLayoutWidget_2)
-        self.pushButton.setObjectName("pushButton")
-        self.pushButton.clicked.connect(self.openWindow)        #HOME버튼 누르면 HOME 화면으로
-        self.horizontalLayout_2.addWidget(self.pushButton)
-        self.verticalLayoutWidget = QtWidgets.QWidget(self.centralwidget)
-        self.verticalLayoutWidget.setGeometry(QtCore.QRect(10, 100, 611, 111))
-        self.verticalLayoutWidget.setObjectName("verticalLayoutWidget")
-        self.verticalLayout = QtWidgets.QVBoxLayout(self.verticalLayoutWidget)
-        self.verticalLayout.setContentsMargins(0, 0, 0, 0)
-        self.verticalLayout.setObjectName("verticalLayout")
-        self.label_2 = QtWidgets.QLabel(self.verticalLayoutWidget)
-        self.label_2.setObjectName("label_2")
-        self.verticalLayout.addWidget(self.label_2)
-        self.verticalLayoutWidget_2 = QtWidgets.QWidget(self.centralwidget)
-        self.verticalLayoutWidget_2.setGeometry(QtCore.QRect(10, 220, 611, 121))
-        self.verticalLayoutWidget_2.setObjectName("verticalLayoutWidget_2")
-        self.verticalLayout_2 = QtWidgets.QVBoxLayout(self.verticalLayoutWidget_2)
-        self.verticalLayout_2.setContentsMargins(0, 0, 0, 0)
-        self.verticalLayout_2.setObjectName("verticalLayout_2")
-        self.label_3 = QtWidgets.QLabel(self.verticalLayoutWidget_2)
-        self.label_3.setObjectName("label_3")
-        self.verticalLayout_2.addWidget(self.label_3)
-        self.verticalLayoutWidget_3 = QtWidgets.QWidget(self.centralwidget)
-        self.verticalLayoutWidget_3.setGeometry(QtCore.QRect(10, 350, 611, 111))
-        self.verticalLayoutWidget_3.setObjectName("verticalLayoutWidget_3")
-        self.verticalLayout_3 = QtWidgets.QVBoxLayout(self.verticalLayoutWidget_3)
-        self.verticalLayout_3.setContentsMargins(0, 0, 0, 0)
-        self.verticalLayout_3.setObjectName("verticalLayout_3")
-        self.label_4 = QtWidgets.QLabel(self.verticalLayoutWidget_3)
-        self.label_4.setObjectName("label_4")
-        self.verticalLayout_3.addWidget(self.label_4)
-        self.verticalScrollBar = QtWidgets.QScrollBar(self.centralwidget)
-        self.verticalScrollBar.setGeometry(QtCore.QRect(620, 10, 20, 451))
-        self.verticalScrollBar.setOrientation(QtCore.Qt.Vertical)
-        self.verticalScrollBar.setObjectName("verticalScrollBar")
-        HighlightWindow.setCentralWidget(self.centralwidget)
-        self.statusbar = QtWidgets.QStatusBar(HighlightWindow)
-        self.statusbar.setObjectName("statusbar")
-        HighlightWindow.setStatusBar(self.statusbar)
+import cv2
+from PyQt5 import QtWidgets
+import pygame
+from moviepy.editor import *
 
-        self.retranslateUi(HighlightWindow)
-        QtCore.QMetaObject.connectSlotsByName(HighlightWindow)
+#파일 경로 가져오기
+path_dir = './video_test'
 
-    def retranslateUi(self, HighlightWindow):
-        _translate = QtCore.QCoreApplication.translate
-        HighlightWindow.setWindowTitle(_translate("HighlightWindow", "Soccer Highlight"))
-        self.label.setText(_translate("HighlightWindow", "가져온 파일 명, 리그 "))
-        self.pushButton.setText(_translate("HighlightWindow", "HOME"))
-        self.label_2.setText(_translate("HighlightWindow", "영상, 시간 "))
-        self.label_3.setText(_translate("HighlightWindow", "영상, 시간 "))
-        self.label_4.setText(_translate("HighlightWindow", "영상, 시간 "))
+file_list = os.listdir(path_dir)        #path에 존재하는 파일 목록
+file_list.sort()
+
+"""
+    파일 이름에서 특정 string을 가지는 파일만 뽑아내기
+"""
+# for item in file_list:
+#     #some_string 이 파일 이름에 없을 경우 -1을 반환
+#     if item.find('#')is not -1:
+#         print(item)
+
+
+class Ui_HighlightWindow(QWidget):
+
+
+    def __init__(self):
+        QWidget.__init__(self, flags=Qt.Widget)
+
+        # 배치될 위젯 변수 선언
+        self.lb_1 = QLabel()
+        self.centralWidget = QtWidgets.QWidget()
+        self.s1 = QtWidgets.QScrollArea(self.centralWidget)
+        self.scrollAreaWidgetContents = QtWidgets.QWidget()
+        self.scrollAreaWidgetContents.setGeometry(QtCore.QRect(0,0,600,600))
+        self.s1.setVerticalScrollBarPolicy(QtCore.Qt.ScrollBarAsNeeded)
+        self.s1.setWidget(self.scrollAreaWidgetContents)
+
+
+        #레이아웃 선언 및 HighlightWindow Widget에 설정
+        self.layout_1 = QBoxLayout(QBoxLayout.TopToBottom, self)    #전체 레이아웃
+        self.layout_2 = QBoxLayout(QBoxLayout.LeftToRight)          #파일 명 위한 레이아웃
+        self.layout_3 = QBoxLayout(QBoxLayout.TopToBottom)          #파일버튼을 위한 레이아웃
+
+        #부모레이아웃에 자식 레이아웃 추가
+        self.layout_1.addLayout(self.layout_2)
+        self.layout_1.addLayout(self.layout_3)
+
+
+        self.setLayout(self.layout_1)
+        self.init_widget()
+
+
+    def init_widget(self):
+        self.setWindowTitle("Soccer Highlight")
+        self.setWindowIcon(QIcon('./images/soccer.png'))
+        self.setFixedWidth(640)
+        self.setFixedHeight(480)
+
+        self.lb_1.setText('파일 명이 들어갈 자리')
+
+        self.lb_1.setStyleSheet("background-color: yellow")
+
+
+
+
+        self.layout_2.addWidget(self.lb_1)
+        self.layout_3.addWidget(self.s1)
+        self.layout_3 = QtWidgets.QVBoxLayout(self.scrollAreaWidgetContents)
+
+
+        #파일 리스트 버튼
+        buttons = {}
+
+        for item in file_list:
+            buttons[item] = QtWidgets.QPushButton(str(item), self)
+            buttons[item].clicked.connect(self.Button)
+            self.layout_3.addWidget(buttons[item])
+
+
+    def Button(self):
+        sender = self.sender()
+        print(sender.text())
+        # 버튼 누르면 파일 재생
+        filename = './video_test/' + sender.text()
+
+        pygame.display.set_caption(sender.text())
+
+        clip = VideoFileClip(filename)
+        clip.preview()
+
+        pygame.quit()
+
 
 
 
 
 if __name__ == "__main__":
     import sys
-    app = QtWidgets.QApplication(sys.argv)
-    HighlightWindow = QtWidgets.QMainWindow()
-    ui = Ui_HighlightWindow()
-    ui.setupUi(HighlightWindow)
-    HighlightWindow.show()
-    sys.exit(app.exec_())
+
+    app = QApplication(sys.argv)
+    form = Ui_HighlightWindow()
+    form.show()
+    exit(app.exec_())
